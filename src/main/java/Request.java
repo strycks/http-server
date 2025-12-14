531a995a464b24f3e1178b5af5ce2ee173dc40ea
Nguyễn Thế Ngọc Hà <88977475+Strkss@users.noreply.github.com>
2025-12-14 22:05:02 +0700

    fixed an issue when there's no user-agent field in request headers

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -10,29 +10,19 @@ public class Request {
   protected MethodType methodType = MethodType.GET;
   protected String requestTarget = "";
   protected String protocol = "";
-  protected String userAgent = "";
+  protected String userAgent = null;
+  protected BufferedReader bufferedReader = null;
 
   /**
    * Constructor with reader from client's socket stream.
    */
   public Request(BufferedReader bufferedReader) throws IOException {
-    List<String> requestContent = new ArrayList<>();
-    String line;
-    // Socket is like an infinite stream (unlike file, which has definite eof)
-    while ((line = bufferedReader.readLine()) != null) {
-      requestContent.add(line);
-      if (line.contains("User-Agent: ")) {
-        userAgent = line.split(" ")[1];
-        break;
-      }
-    }
-
-    if (requestContent.isEmpty()) {
+    this.bufferedReader = bufferedReader;
+    String line = bufferedReader.readLine();
+    if (line == null) {
       return;
     }
-
-    System.out.println("requestContent: " + requestContent);
-    String[] requestLineArgs = requestContent.getFirst().split(" ");
+    String[] requestLineArgs = line.split(" ");
     methodType = MethodType.valueOf(requestLineArgs[0]);
     requestTarget = requestLineArgs[1];
     protocol = requestLineArgs[2];
@@ -50,7 +40,17 @@ public class Request {
     return protocol;
   }
 
-  public String getUserAgent() {
+  /**
+   * return userAgent or read until userAgent appears.
+   */
+  public String getUserAgent() throws IOException {
+    if (userAgent == null && bufferedReader != null) {
+      String line = bufferedReader.readLine();
+      while (!line.contains("User-Agent: ")) {
+        line = bufferedReader.readLine();
+      }
+      userAgent = line.split(" ")[1];
+    }
     return userAgent;
   }
 }
